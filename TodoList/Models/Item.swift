32980517dde84c1cc9ba64de3366f0b1103b7920
ryantstone <ryan@ryanstone.io>
2018-10-07 20:32:58 -0400

    Update Item to accommodate cloudkit

--- TodoList/Models/Item.swift
+++ TodoList/Models/Item.swift
@@ -3,26 +3,26 @@ import CloudKit
 
 class Item: Codable {
     let title: String
-    var recordId: CKRecordID?
+    var recordName: String?
     var isComplete: Bool
     var record: CKRecord
     
     init(title: String, isComplete: Bool = false, record: CKRecord?) {
         self.title = title
         self.isComplete = isComplete
-        if let record = record {
+        if let record   = record {
             self.record = record
         } else {
-            self.record = CKRecord(recordType: "item")
-            self.record["title"] = self.title
-            self.record["isComplete"] = self.isComplete
+            self.record                 = CKRecord(recordType: "item")
+            self.record["title"]        = self.title
+            self.record["isComplete"]   = self.isComplete
         }
     }
     
     public func save() {
         CloudKitService.init().save(record: record) { (record) in
-            self.record = record
-            self.recordId = record.recordID
+            self.record     = record
+            self.recordName = record.recordID.recordName
         }
     }
     
@@ -33,13 +33,13 @@ class Item: Codable {
     }
     
     required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        title = try container.decode(String.self, forKey: .title)
-        isComplete = try container.decode(Bool.self, forKey: .isComplete)
+        let container   = try decoder.container(keyedBy: CodingKeys.self)
+        title           = try container.decode(String.self, forKey: .title)
+        isComplete      = try container.decode(Bool.self, forKey: .isComplete)
         
-        self.record = CKRecord(recordType: "item")
-        self.record["title"] = self.title
-        self.record["isComplete"] = self.isComplete
+        self.record                 = CKRecord(recordType: "item")
+        self.record["title"]        = self.title
+        self.record["isComplete"]   = self.isComplete
     }
     
     func encode(to encoder: Encoder) throws {
